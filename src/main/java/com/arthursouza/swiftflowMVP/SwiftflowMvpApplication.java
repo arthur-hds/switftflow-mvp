03ecd397a63f6500888b5d88e124fdaa046279a2
arthur_hds <arthurhenrique.thor@gmail.com>
2024-03-06 23:54:09 -0300

    first commit

---
+++ src/main/java/com/arthursouza/swiftflowMVP/SwiftflowMvpApplication.java
@@ -0,0 +1,13 @@
+package com.arthursouza.swiftflowMVP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftflowMvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftflowMvpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arthursouza/swiftflowMVP/SwiftflowMvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthursouza.swiftflowMVP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftflowMvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
