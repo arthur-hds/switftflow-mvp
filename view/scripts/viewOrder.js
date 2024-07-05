4e53f435992fe3f52e2be6baaeba3854ab94fe6a
arthur_hds <arthurhenrique.thor@gmail.com>
2024-07-04 21:13:49 -0300

    refactor: Changed elements to Order Item View

--- view/scripts/viewOrder.js
+++ view/scripts/viewOrder.js
@@ -1,102 +1,36 @@
-const url = "http://localhost:8080/"
-let CurrentColumn = ""
-let CurrentColumnOrigin = ""
+//--------------Global Variables--------------
 
-//--------------Functions to get and set data at table--------------
-function show(data){
+let SelectedList = [];
+let ClientsList = [];
+let Values = []
+let tab = ``;
 
-    console.log("Data: ", data )
+//--------------------------------------------
 
-    if(data.length > 0){  // Checks if the response is null or not
-        let tab =
-        `<thead>`;
-    
-        const sampleData = data[0] !== undefined ? data[0] : data;  //Returns the Table JSON
-    
-        for (let column in sampleData){
-    
-            tab += `
-            <th scope="col">${column}</th>
-            `
-        }
-    
-        tab += 
-        `
-            </thead>
-        `;
-    
-    
-        function renderRow(value){  //Create tables
-    
-            tab += `
-                <tr>`;
-    
-            for(let column in value){ 
-    
-                if(typeof value[column] === "object" && value[column] !== null){
-                    
-                    console.log("Column::: ", value[column])
 
-                    tab+= `<td scope="row"> ${value[column].id} - ${value[column].name !== undefined ? value[column].name : value[column].team_id.name}  </td>`
-    
-                }else{
-    
-                    tab+= `<td scope="row"> ${value[column]} </td>`;
-                
-                }
-            }
-    
-            tab +=`  
-                </tr>
-            `;
-        }
-    
-    
-    
-        if(data[0] === undefined){  //Solo JSON
-    
-            renderRow(data);
-        
-        }else{                      //Array JSON
-    
-            for(let j = 0; j< Object.values(data).length; j++){  //Populate every row
-    
-                renderRow(data[j]);
-    
-            }
-        }
-    
-    
-        document.getElementById("table").innerHTML = tab;       //Populate the table div
-        document.getElementById("items-content").innerHTML = ``;  //Clears the order div
+//--------------Function to get the JSON data--------------
+async function getApi(url) {
+    const response = await fetch(url, {
+        method: "GET"
+    })
 
-        
-    }else{
-
-        let tab = `<i class="bi bi-emoji-dizzy" style="font-size: 10rem; color: #5f4d8a;"></i>`;
-        tab += `<h2 style="color: #5f4d8a;"> There is no data avaiable here... </h2>`
-        
-        document.getElementById("items-content").innerHTML = tab;  //Adds the following error alert content
-        
-
-    }
-    
+    const data = await response.json();
+    return data
 
 }
 
-//--------------Function to pull the db content--------------
-async function getAPI(url){
 
-    const response = await fetch(url, {     // Gets the response body
-        method: "GET"});     
+//--------------Function to get the Session Storage items--------------
+function getData() {
 
-    
-    const data = await response.json();  
-    
-    console.log(Object.values(data).length);
-    
-    return data;
+    SelectedList = JSON.parse(sessionStorage.getItem("Orders"));
+    ClientsList = JSON.parse(sessionStorage.getItem("Clients"));
+    Values = JSON.parse(sessionStorage.getItem("Values"));
+
+    console.log(ClientsList[0])
+
 }
+
 
 //--------------Function to change HTML when back arrow pressed--------------
 function changeWindow(page) {
@@ -106,15 +40,182 @@ function changeWindow(page) {
 }
 
 
-//--------------Function to get the api value and pass to table--------------
-async function fetchDataAndShow() {
-    try {
-        const data = await getAPI(url + "orders");
-        show(data);
-    } catch (error) {
-        console.error("Error at getting data:", error);
+//--------------Function to populate the div with Session Storage items--------------
+function loadUpOrder(data) {
+
+
+
+
+
+    function loadUpSelected(shirtJson) {
+
+        tab += `
+        <div class="providers">
+            <h2>${shirtJson.client}</h2>
+            <h4 id="${shirtJson.team}-${shirtJson.id}">${shirtJson.team} - ${shirtJson.type} - ${shirtJson.season}</h4>
+            <div class="providers__price">
+                <h4 id="${shirtJson.team}-${shirtJson.id}">Price: R$${shirtJson.price}</h4>
+                <h4 id="${shirtJson.team}-${shirtJson.id}">Revenue: R$${shirtJson.revenue}</h4>
+            </div> 
+            
+        </div>
+
+        `
     }
+
+
+
+    for (let i of data) {
+
+        loadUpSelected(i);
+
+
+    }
+
+    const container = document.getElementById("items-container");
+
+    container.innerHTML = tab;
+
+
 }
 
-fetchDataAndShow()
 
+
+//--------------Function to set details of the main order--------------
+function loadUpDetails() {
+    const clientsText = document.getElementById("clientsQuantity");
+    const shirtsText = document.getElementById("shirtsQuantity");
+    const providerText = document.getElementById("provider");
+    const costText = document.getElementById("costValue");
+    const profitText = document.getElementById("profitValue");
+
+
+    costText.textContent = "R$ " + Values.cost;
+    profitText.textContent = "R$ " + Values.profit
+    providerText.textContent = SelectedList[0].provider;
+    shirtsText.textContent = SelectedList.length;
+    clientsText.textContent = ClientsList.length;
+
+}
+
+
+
+//--------------Function create main order--------------
+async function createOrder() {
+
+    const url = "http://localhost:8080/orders"
+
+    const AllParams = {  //Interface and JSON to Order row
+
+        "status": false,
+        "total": Values.cost,
+        "provider_id": { "id": SelectedList[0].providerID }
+
+    }
+
+
+    try {
+        const response = await fetch(url, {
+
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(AllParams)
+
+        })
+
+        if (!response.ok) {
+            throw new Error("Error in the process of sending data.");
+        }
+
+        const data = await response;
+        console.log("Success: ", data);
+
+
+    } catch (error) {
+
+        console.error("Erro: ", error);
+
+    }
+
+
+}
+
+
+
+//--------------Function create each items of the order--------------
+async function createOrderItem() {
+
+    const url = "http://localhost:8080/orders/latest"
+    const urlPost = "http://localhost:8080/orderItem"
+    const latestCreatedOrder = await getApi(url);
+
+
+    async function postOrderItem(AllParams){
+        try {
+            const response = await fetch(urlPost, {
+
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(AllParams)
+
+            })
+
+            if (!response.ok) {
+                throw new Error("Error in the process of sending data.");
+            }
+
+            const data = await response;
+            console.log("Success: ", data);
+
+
+        } catch (error) {
+
+            console.error("Erro: ", error);
+
+        }
+    }
+
+
+    for (let i of SelectedList) {
+
+        let orderItemJSON =
+        {
+            "orders_id": {
+                "id": latestCreatedOrder.id
+            },
+            "disponibility_id": {
+                "id": i.disponibilityID
+            },
+            "orderClient": {
+                "id": i.orderClientID 
+            }
+        }
+
+        postOrderItem(orderItemJSON)
+
+    }
+
+
+
+
+}
+
+
+//--------------Function create both orders in sequence--------------
+async function createOrders(){
+
+    await createOrder();
+    await createOrderItem();
+    changeWindow('index');
+
+}
+
+
+
+getData();
+loadUpOrder(SelectedList);
+loadUpDetails();
---
+++ view/scripts/viewOrderItem.js
@@ -0,0 +1,120 @@
+const url = "http://localhost:8080/"
+let CurrentColumn = ""
+let CurrentColumnOrigin = ""
+
+//--------------Functions to get and set data at table--------------
+function show(data){
+
+    console.log("Data: ", data )
+
+    if(data.length > 0){  // Checks if the response is null or not
+        let tab =
+        `<thead>`;
+    
+        const sampleData = data[0] !== undefined ? data[0] : data;  //Returns the Table JSON
+    
+        for (let column in sampleData){
+    
+            tab += `
+            <th scope="col">${column}</th>
+            `
+        }
+    
+        tab += 
+        `
+            </thead>
+        `;
+    
+    
+        function renderRow(value){  //Create tables
+    
+            tab += `
+                <tr>`;
+    
+            for(let column in value){ 
+    
+                if(typeof value[column] === "object" && value[column] !== null){
+                    
+                    console.log("Column::: ", value[column])
+
+                    tab+= `<td scope="row"> ${value[column].id} - ${value[column].name !== undefined ? value[column].name : value[column].team_id.name}  </td>`
+    
+                }else{
+    
+                    tab+= `<td scope="row"> ${value[column]} </td>`;
+                
+                }
+            }
+    
+            tab +=`  
+                </tr>
+            `;
+        }
+    
+    
+    
+        if(data[0] === undefined){  //Solo JSON
+    
+            renderRow(data);
+        
+        }else{                      //Array JSON
+    
+            for(let j = 0; j< Object.values(data).length; j++){  //Populate every row
+    
+                renderRow(data[j]);
+    
+            }
+        }
+    
+    
+        document.getElementById("table").innerHTML = tab;       //Populate the table div
+
+
+        
+    }else{
+
+        let tab = `<i class="bi bi-emoji-dizzy" style="font-size: 10rem; color: #5f4d8a;"></i>`;
+        tab += `<h2 style="color: #5f4d8a;"> There is no data avaiable here... </h2>`
+        
+        document.getElementById("items-content").innerHTML = tab;  //Adds the following error alert content
+        
+
+    }
+    
+
+}
+
+//--------------Function to pull the db content--------------
+async function getAPI(url){
+
+    const response = await fetch(url, {     // Gets the response body
+        method: "GET"});     
+
+    
+    const data = await response.json();  
+    
+    console.log(Object.values(data).length);
+    
+    return data;
+}
+
+//--------------Function to change HTML when back arrow pressed--------------
+function changeWindow(page) {
+
+
+    window.location.href = `${page}.html`;
+}
+
+
+//--------------Function to get the api value and pass to table--------------
+async function fetchDataAndShow() {
+    try {
+        const data = await getAPI(url + "orders");
+        show(data);
+    } catch (error) {
+        console.error("Error at getting data:", error);
+    }
+}
+
+fetchDataAndShow()
+
