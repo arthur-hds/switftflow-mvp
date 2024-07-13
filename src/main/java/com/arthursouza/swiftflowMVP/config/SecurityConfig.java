4fdcb33b4dce6c2e01175cad692858f338ee6de3
arthur_hds <arthurhenrique.thor@gmail.com>
2024-07-12 22:43:18 -0300

    feature: Added authenticationManager to build up the UserDetails Service

--- src/main/java/com/arthursouza/swiftflowMVP/config/SecurityConfig.java
+++ src/main/java/com/arthursouza/swiftflowMVP/config/SecurityConfig.java
@@ -2,13 +2,17 @@ package com.arthursouza.swiftflowMVP.config;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -19,6 +23,15 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+
+
+    private AuthenticationManager authenticationManager;
+
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+
 
     public static final String[] PUBLIC_MATCHERS = {
 
@@ -35,9 +48,22 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
+        
         // Disable the "csrf" protection, usable for test the program
         http.cors().and().csrf().disable();
+
+
+        AuthenticationManagerBuilder authenticationManagerBuilder = http
+            .getSharedObject(AuthenticationManagerBuilder.class);
+
+        
+        authenticationManagerBuilder.userDetailsService(this.userDetailsService)
+            .passwordEncoder(bCryptPasswordEncoder());
+
+
+        this.authenticationManager = authenticationManagerBuilder.build();
+
+
 
         // Disable the process of authentication from the following endpoints
         http.authorizeHttpRequests()
