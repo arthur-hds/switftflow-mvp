994ba0e841a04be573e86c3cd879e832798c8e89
arthur_hds <arthurhenrique.thor@gmail.com>
2024-07-27 23:28:14 -0300

    feature: Added login functions, so it can validate the user from the input values and set it to the local storage

--- view/scripts/login.js
+++ view/scripts/login.js
@@ -1,4 +1,45 @@
-function onLogIn(){
-    console.log(123)
+//--------------Function login into user--------------
+async function login() {
 
+    const username = document.getElementById("user").value;
+    const password = document.getElementById("password").value;
+
+
+    const response = await loginApi(username, password)
+
+    let key = "Authorization";
+    
+    let token = response.headers.get(key);
+    window.localStorage.setItem(key, token);
+
+    changeWindow("index");
+
+
+}
+
+//--------------Function to call the login method from the API--------------
+async function loginApi(username, password){
+
+    const response = await fetch("http://localhost:8080/login", {
+        method: "POST",
+        headers: new Headers({
+            "Content-Type": "application/json; charset=utf8",
+            Accept: "application/json",
+        }),
+        body: JSON.stringify({
+            username: username,
+            password: password,
+        }),
+    });
+
+
+    return response;
+
+}
+
+
+//--------------Function to change HTML when back arrow pressed--------------
+function changeWindow(page) {
+
+    window.location.href = `${page}.html`;
 }
